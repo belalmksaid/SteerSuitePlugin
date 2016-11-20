67b40dd6de64bdcfa1e3380702fa453bf2252dbb
belalmksaid <belalmksaid@mgmail.com>
2016-11-20 12:04:50 -0500

    more files

---
+++ SteerSuitePlugin/DataStructures/Graph.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteerSuitePlugin.DataStructures
+{
+    public class Graph<T>
+    {
+        List<Vertex<T>> vertices;
+        bool[,] edges = new bool[10,10];
+    }
+}
---
+++ SteerSuitePlugin/DataStructures/Vertex.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteerSuitePlugin.DataStructures
+{
+    public class Vertex<T>
+    {
+        private T data;
+        private LinkedList<Vertex<T>> neighbors;
+    }
+}
